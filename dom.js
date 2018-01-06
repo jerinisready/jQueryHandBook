6832236708b75b5b07dc810ca354539e529e64bf
Jerin <jerinisready@gmail.com>
2018-01-07 01:39:08 +0530

    INIT

---
+++ dom.js
@@ -0,0 +1,84 @@
+DOM STRUCTURE
+document
+elements
+style
+events
+attributes
+
+
+------------------------------------------
+document
+// https://www.w3schools.com/jsref/dom_obj_document.asp
+document.addEventListener()
+document.createAttribute()
+document.createElement()
+document.createTextNode()
+
+
+
+------------------------------------------
+Elements
+// https://www.w3schools.com/jsref/dom_obj_all.asp
+element.innerHTML = "My Javascript"
+element.addEventListener()
+element.childNodes
+element.click()
+element.getElementsByClassName()
+element.getElementsByTagName()
+element.hasAttribute()
+element.innerHTML
+element.setAttribute()
+
+
+------------------------------------------
+Style
+// https://www.w3schools.com/jsref/dom_obj_style.asp
+object.style.background = ""
+document.getElementById("myH1").style.color = "red";
+
+------------------------------------------
+Events in Javascript
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+var x = document.getElementById("myBtn");
+x.addEventListener("click", myFunction);
+    onclick | ondblclick | onmouseover | onkeypress
+    onerror | onpageshow | onscroll | oninput
+    onsubmit | ondragover | oncopy | onpaste | ononline | onoffline ...
+
+  Methods
+    preventDefault()
+    stopImmediatePropagation()
+    stopPropagation()
+
+  Keyboard Special Events
+    <body onmousedown="isKeyPressed(event)">
+    ctrlKey | altKey | shiftKey | metaKey
+    <script>
+    function isKeyPressed(event) {
+        if (event.ctrlKey) {
+            alert("The CTRL key was pressed!");
+        } else {
+            alert("The CTRL key was NOT pressed!");
+        }
+    }
+    </script>
+------------------------------------------
+
+attributes
+// https://www.w3schools.com/jsref/dom_obj_attributes.asp
+<button onclick="javascript:void(0);">
+var btn = document.getElementsByTagName("BUTTON")[0];
+btn.attributes.getNamedItem("onclick").value;
+attr.name
+attr.value
+attr.specified
+------------------------------------------
+
+
+
+BOM STRUCTURE
+window
+window.location
+window.history
+window.screen
+window.navigator
---
+++ js-intro.js
@@ -0,0 +1,124 @@
+JS can 2 two things,
+        1) Alter anything in HTML DOM
+        2) Communicate with External World!
+------------------------------------------
+
+Functions
+  function name(parameter1, parameter2, parameter3, ...) { code to be executed  }
+  name(parameter1, parameter2, parameter3, ...);
+  sum =   function (x,y) {
+              return x+y;
+          };
+
+------------------------------------------
+Variables Datatypes Objects and  typeof
+  var x;      |
+  Number()    |   456 , 46864, 4.025, 0.0000056
+  String()    |   "Text"  ,  'String'
+  null        |   NaN
+  undefined   |
+  function    |   function f(a,b) {return a+b;}
+  Array()     |   [
+              |    0,
+              |    "hello",
+              |    false,
+              |    (1>0),
+              |    [y, x],
+              |    {'key1':'value', 'key2': 'value2'},
+              |    function(x){return x*x;}
+              |  ]
+  Object()    |   {
+              |    'key1':'value',
+              |    'key2': 55,
+              |    'isthere': true,
+              |    'marks':[],
+              |    'fun': function(x){return x*x;}
+              |  }
+  Boolean()   |   true , false
+------------------------------------------
+  Objects
+  var x = {
+    'firstName'   : 	'John',
+    'lastName'    : 	'Doe',
+    'age'         : 	50,
+    'eyeColor'    : 	'blue',
+    'fullName'    : 	function() {return this.firstName + " " + this.lastName;}
+  }
+
+  // called by :::
+  //  x.lastName
+  //  x['eyeColor']
+  // Accessing Objects : x.fullName()
+
+------------------------------------------
+Global and Local Variables
+var x = 10
+function(y){
+  var z = 20;
+  return x * y + z;
+}
+// using z will through error here
+
+------------------------------------------
+Conditions  and Switches
+    if(hour < 12){
+      alert('Good Morning');
+    } elseif( hour < 18){
+      alert('Good Evening');
+    } else{
+      alert('Good Night');
+    }
+
+  var ch = 25
+  switch(ch) {
+      case "hello":
+          console.log("Adress Selected");
+          break;
+      case 25:
+          console.log("Phone Number Selected");
+          break;
+      default:
+        console.log("Enter Valid input");
+  }
+------------------------------------------
+
+Loops
+4 types of Loops
+    1)  for(initialiation; condition; updation){ Loop }
+    2)  for (item in items) { Loop }
+    3)  while(condition) { Loop }
+    4)  do { Loop } while(condition);
+
+------------------------------------------
+String Handling Functions
+// https://www.w3schools.com/jsref/jsref_obj_string.asp
+------------------------------------------
+charAt() | concat() | endsWith() | includes() | indexOf() | match() | replace()
+search() | split() | slice() |  toUpperCase() | trim()  ...
+
+String Style Modification Functions
+anchor()  |  bold() | fontsize() | italics() | small() | sub() ...
+
+------------------------------------------
+Number Handling Functions
+NaN | MAX_VALUE | MIN_VALUE | POSITIVE_INFINITY | NEGATIVE_INFINITY | prototype
+isFinite() | isInteger() | isNaN() | toFixed(x) | toString() |  valueOf() ...
+
+Array Handling Functions
+concat() | every() | find() | forEach() | isArray() | join() | map() ...
+// https://www.w3schools.com/jsref/jsref_obj_array.asp
+
+------------------------------------------
+Tokens
+    break
+    continue
+    debugger
+    do ... while
+    for
+    function
+    if ... else
+    return
+    switch ... cases
+    try ... catch
+    var
+------------------------------------------

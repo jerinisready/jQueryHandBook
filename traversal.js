fb5ff0df307f3bf3db7a394c71dd78a611cc313b
JK <jerinisready@gmail.com>
2018-01-25 14:56:09 +0530

    Update traversal.js

--- traversal.js
+++ traversal.js
@@ -10,7 +10,7 @@ $("ul li:first") 	         -     Selects the first <li> element of the first <ul
 $("ul li:first-child") 	         -     Selects the first <li> element of every <ul>
 $("[href]") 	         -     Selects all elements with an href attribute
 $("a[target='_blank']") 	         -     Selects all <a> elements with a target attribute value equal to "_blank"
-$("a[target!='_blank']") 	         -     Selects all <a> elements with a target attri
+$("a[target!='_blank']") 	         -     Selects all <a> elements with a target attribute value not equal to "_blank"
 $(":button") 	         -     Selects all <button> elements and <input> elements of type="button"
 $("tr:even") 	         -     Selects all even <tr> elements
 $("tr:odd") 	         -     Selects all odd <tr> elements
